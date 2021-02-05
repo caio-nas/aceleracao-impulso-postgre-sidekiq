f4b72541ad840d9272c2e4971ac1474c6cec8dcf
Caio Nascimento <contato@caionascimento.com.br>
2021-02-05 11:45:33 -0300

    Setup upsert_all_spec

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # Aceleração Global Dev #5 Impulso
-## Tema: Integrando Ruby On Rails Com Banco PostgreSQL e Sidekiq
+## Subject: Advanced integrations for Ruby On Rails, PostgreSQL and Sidekiq
 
-Como profilar os testes:
+## Hosts
+
+- Caio Nascimento - https://github.com/caio-nas - https://www.linkedin.com/in/caiomvnascimento/
+- Lucas Santos - https://github.com/lsantosc - https://www.linkedin.com/in/lsantosc/
+
+## Topics
+
+### insert_all/upsert_all
+New Rails 6 methods for bulk operations
+
+- spec/database/insert_all_spec.rb
+- spec/database/upsert_all_spec.rb
+
+## How to run profiler tests:
 
 ```
   RAILS_ENV=test bundle exec rspec -fd --profile
+
+  # Docker setup is available for your convenience <3
+  docker-compose run web RAILS_ENV=test bundle exec rspec -fd --profile
 ```
+
+## References
+- https://bigbinary.com/blog/bulk-insert-support-in-rails-6
---
+++ spec/database/upsert_all_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+require 'csv'
+
+# upsert_all was introduced on Rails 6.
+# For previous versions, try https://github.com/zdennis/activerecord-import
+RSpec.describe '#upsert_all' do
+  fixtures :brands
+  let(:file) { file_fixture('cars.csv').read }
+  let(:csv)  { csv = CSV.parse(file, headers: true) }
+  let(:duplication_size) { 5 }
+
+  before do
+    original = car_hashes_from_csv
+      .sample(duplication_size)
+      .map{ |car| car.update(name: "[ORIGINAL] #{car[:name]}") }
+
+    Car.insert_all!(original)
+  end
+
+  context 'comparing with iteration' do
+    context 'iteration' do
+      it do
+        expect {
+          expect(unchanged_entries).to eq(duplication_size)
+
+          car_hashes_from_csv.each do |car_attrs|
+            Car.upsert(car_attrs)
+          end
+
+          expect(unchanged_entries).to eq(0)
+        }.to perform_under(5).sec.warmup(0).sample(1)
+      end
+    end
+
+    context 'upsert_all' do
+      it do
+        expect {
+          expect(unchanged_entries).to eq(duplication_size)
+
+          Car.upsert_all(car_hashes_from_csv)
+
+          expect(unchanged_entries).to eq(0)
+        }.to perform_under(1).sec.warmup(0).sample(1)
+      end
+    end
+  end
+
+  private
+
+  def brands
+    @brands ||= Hash[Brand.all.map{ |b| [b.name, b] }]
+  end
+
+  def unchanged_entries
+    Car.where("name LIKE '[ORIGINAL]%'").count
+  end
+
+  def car_hashes_from_csv
+    @car_hashes_from_csv = csv.map do |row|
+      next if row['brand'].blank?
+      { id: row['id'], name: row['name'], brand_id: brands[row['brand'].strip].id }
+    end.compact
+  end
+end
